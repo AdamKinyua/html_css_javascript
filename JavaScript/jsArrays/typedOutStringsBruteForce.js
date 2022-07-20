240e322ffe758de57e00c8720a5ad9d50523448c
adam_kinyua <85613991+AdamKinyua@users.noreply.github.com>
2022-07-20 16:28:24 -0400

    typed out strings brute force

---
+++ JavaScript/jsArrays/typedOutStringsBruteForce.js
@@ -0,0 +1,21 @@
+//Q: Given two strings S and T, return if they are equal when both are typed out. Any '#' that appears in the string counts as a backspace
+
+const typedOutStrings = (S,T) => {
+  var sumT = 0, sumS = 0;
+  for(let i=0; i<S.length; i++) {
+    if(S[i] === "#") {
+      sumS -= 1;
+    } else {sumS += 1 }
+    
+  }
+  for(let j=0; j<T.length; j++) {
+    if(T[j] === "#") {
+      sumT -= 1;
+    } else {sumT += 1 }
+  }
+  return [sumT, sumS];
+} 
+
+S = "abc##def";
+T = "smhd";
+console.log(typedOutStrings(S,T))
